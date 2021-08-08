9eda6d93daa9da8459c62fcec22fe814312acb71
yadav42 <yadav4shrestha@gmail.com>
2021-08-08 22:51:01 +0545

    Initial commit on validations.py

---
+++ validations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def validate_user(username,minlen):
+    """Checks if the recieved username matches the required condtions."""
+    if type(username)!=str:
+        raise TypeError("username must be a string")
+    if minlen  < 1:
+        raise ValueError("minlen must be at least 1")
+    if len(username)<minlen:
+        return False
+    if not username.isalnum():
+        return False
+
+    #username cannont begin with a number
+    if username[0].isnumeric():
+        return False
+    return True
